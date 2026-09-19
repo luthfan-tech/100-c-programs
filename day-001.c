40083a0c184f99224b9fe44de6c284a57fcb1dcb
luthfan-tech <luthfanmak@gmail.com>
2026-09-19 10:18:54 +0530

    feat: initial commit with folder structure and day 1 hello world

---
+++ day-001.c
@@ -0,0 +1,13 @@
+//Program 001: Hello World
+
+#include <stdio.h>
+
+int main(void) {
+    printf("hello world\n");
+    printf("Starting my 100 days of code challenge in C language\n");
+    return 0;
+}
+
+//Expected output:
+/*Hello, World!
+Starting my 100 days of C programming.*/
