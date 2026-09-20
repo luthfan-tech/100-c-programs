5580df6f4be5a6001c176815f577bc4700bf0540
luthfan-tech <luthfanmak@gmail.com>
2026-09-20 13:02:38 +0530

    feat: add program 002 personal info

---
+++ day-002.c
@@ -0,0 +1,17 @@
+//Program 002: Print Personal Information
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Name: Lutfan\n"
+           "Degree/branch: Computer Science\n"
+           "Year: 1st year\n"
+           "College/goal: Dr. T. Thimmaiah Institute of Technology\n");
+    return 0;
+}
+
+//Expected output:
+/*Name: Lutfan
+Degree/branch: Computer Science
+Year: 1st year
+College/goal: Dr. T. Thimmaiah Institute of Technology*/
