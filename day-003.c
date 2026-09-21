0a5a4f54f04d9cd228e5ebbd4e84ce05be823233
luthfan-tech <luthfanmak@gmail.com>
2026-09-21 18:44:07 +0530

    feat: add program 003 ascii art

---
+++ day-003.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    /* Centered 5-level star pyramid */
+    printf("    *\n"
+           "   ***\n"
+           "  *****\n"
+           " *******\n"
+           "*********\n");
+    return 0;
+}
